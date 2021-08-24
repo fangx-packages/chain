b26378649561092fddd78e14d47c67441962b0f4
nfangxu <nfangxu@gmail.com>
2021-08-24 16:41:52 +0800

    feat: Use New() instead of NewChain()

--- chain.go
+++ chain.go
@@ -2,7 +2,7 @@ package chain
 
 type Chain func(next interface{}) interface{}
 
-func NewChain(v ...Chain) Chain {
+func New(v ...Chain) Chain {
 	return func(next interface{}) interface{} {
 		if len(v) > 0 {
 			for i := len(v) - 1; i >= 0; i-- {
